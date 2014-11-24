f448e584aa4ee355aec90b27198c54a63125a5fe
Daniel DOUAT <daniel.douat@aelys-info.fr>
2014-11-24 22:05:04 +0100

    updated code to support new timply vers

---
+++ scriptsEnabled/005-page.php
@@ -0,0 +1,24 @@
+<?php
+function page1()
+{
+    global $object;
+    $template = new timply('page.html');
+    $object->setTemplate($template);
+}
+function page2()
+{
+    global $stack, $object;
+    $template = $object->getTemplate();
+    $template->setElement('content', MarkdownExtra::defaultTransform($object->getInputContent()));
+    $object->setOutputContent($template->returnHtml());
+}
+function page3()
+{
+    global $stack;
+    foreach ($stack->getStack() as $key => $object) {
+        if ($object->getMetas()->type === 'page') {
+            file_put_contents($object->getOutputUri(), $object->getOutputContent());
+        }
+    }
+}
+?>
