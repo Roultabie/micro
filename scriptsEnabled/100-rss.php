cf9bce2fc513f6895489d826ca62bbb7c00a5897
Daniel Douat <daniel.douat@aelys-info.fr>
2014-11-25 14:28:40 +0100

    updated rss script

--- scriptsEnabled/100-rss.php
+++ scriptsEnabled/100-rss.php
@@ -4,21 +4,21 @@ function rss4()
     global $stack;
 
     //$maxItems = (!empty($GLOBALS['maxItems'])) ? $GLOBALS['maxItems'] : 10;
-    if (REGEN === false) {
-        $feedUri  = PUBLIC_PATH . '/feed.rss';
-        $template = new timply('feed.rss');
-        $template->setElement('siteTitle', $GLOBALS['siteTitle']);
-        $template->setElement('siteLink', $GLOBALS['siteTitle']);
-        $template->setElement('siteDescription', $GLOBALS['siteTitle']);
-        foreach ($stack->getStack() as $key => $object) {
-            if (is_object($object)) {
-                $template->setElement('itemTitle', $object->getMetas()->title, 'Item');
-                $template->setElement('itemLink', $object->getOutputUrlAbs(), 'Item');
-                $template->setElement('itemDescription', MarkdownExtra::defaultTransform($object->getInputContent()), 'Item');
-                $template->setElement('itemPubDate', $date, 'Item');
-                $template->setElement('itemGuid', $object->getOutputUrlAbs(), 'Item');
-            }
+    $feedUri  = PUBLIC_PATH . '/feed.rss';
+    $template = new timply('feed.rss');
+    $template->setElement('siteTitle', $GLOBALS['siteTitle']);
+    $template->setElement('siteLink', $GLOBALS['siteTitle']);
+    $template->setElement('siteDescription', $GLOBALS['siteTitle']);
+    foreach ($stack->getStack() as $key => $object) {
+        if (is_object($object)) {
+            $template->setElement('itemTitle', $object->getMetas()->title, 'Item');
+            $template->setElement('itemLink', $object->getOutputUrlAbs(), 'Item');
+            $template->setElement('itemDescription', MarkdownExtra::defaultTransform($object->getInputContent()), 'Item');
+            $template->setElement('itemPubDate', $date, 'Item');
+            $template->setElement('itemGuid', $object->getOutputUrlAbs(), 'Item');
         }
+    }
+    if (REGEN === false) {
         if (file_exists($feedUri)) {
             $oldRss = simplexml_load_file($feedUri);
             foreach ($oldRss->channel->item as $elements) {
@@ -29,7 +29,7 @@ function rss4()
                 $template->setElement('itemGuid', $elements->guid, 'Item');
             }
         }
-        file_put_contents($feedUri, $template->returnHtml());
     }
+    file_put_contents($feedUri, $template->returnHtml());
 }
 ?>
